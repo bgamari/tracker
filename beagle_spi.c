1c50e60cc99313b539a1b773e601d841b6792157
Ben Gamari <bgamari.foss@gmail.com>
2012-11-09 19:39:26 -0500

    beagle_spi: Get it working
    SPI mode 0 seems to work fine

--- beagle_spi.c
+++ beagle_spi.c
@@ -6,16 +6,11 @@
 
 #include "beagle_spi.h"
 
-#define BUFFER_LEN 256
+static bool tx_busy = false;
 
-static uint8_t buffer[BUFFER_LEN];
-static unsigned int offset;
-
-enum state_t {
-    IDLE, WAIT_FOR_LENGTH,
-};
-
-enum state_t state = IDLE;
+static bool rx_busy = false;
+static void* rx_user_data;
+static completion_t rx_completion;
 
 void beagle_spi_init()
 {
@@ -23,15 +18,12 @@ void beagle_spi_init()
     gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO4 | GPIO5 | GPIO6 | GPIO7);
     gpio_set_af(GPIOA, GPIO_AF5, GPIO4 | GPIO5 | GPIO6 | GPIO7);
 
-    offset = 0;
-
     // Turn things on
     rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_SPI1EN);
     rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_DMA2EN);
 
     nvic_enable_irq(NVIC_SPI1_IRQ);
     spi_reset(SPI1);
-    spi_enable_rx_buffer_not_empty_interrupt(SPI1);
     spi_set_slave_mode(SPI1);
     spi_enable(SPI1);
 
@@ -45,6 +37,10 @@ void beagle_spi_init()
     dma_set_transfer_mode(DMA2, 5, DMA_SCR_DIR_MEM2PER);
     dma_enable_memory_increment_mode(DMA2, 5);
     dma_set_peripheral_address(DMA2, 5, (uint32_t) &SPI_DR(SPI1));
+    dma_enable_fifo_mode(DMA2, 5);
+    dma_enable_transfer_error_interrupt(DMA2, 5);
+    dma_enable_transfer_complete_interrupt(DMA2, 5);
+    dma_enable_fifo_error_interrupt(DMA2, 5);
 
     // RX: DMA2_Stream0
     dma_stream_reset(DMA2, 0);
@@ -52,21 +48,61 @@ void beagle_spi_init()
     dma_set_transfer_mode(DMA2, 0, DMA_SCR_DIR_PER2MEM);
     dma_enable_memory_increment_mode(DMA2, 0);
     dma_set_peripheral_address(DMA2, 0, (uint32_t) &SPI_DR(SPI1));
-    dma_set_memory_address(DMA2, 0, (u32) buffer);
+    dma_enable_fifo_mode(DMA2, 0);
+    dma_enable_transfer_error_interrupt(DMA2, 0);
+    dma_enable_transfer_complete_interrupt(DMA2, 0);
+    dma_enable_fifo_error_interrupt(DMA2, 0);
 }
 
-void spi1_isr()
+int beagle_spi_dma_tx(uint16_t length, char *buffer)
 {
-    spi_read(SPI1);
+    if (tx_busy) return -1;
+    tx_busy = true;
+    spi_enable_tx_dma(SPI1);
+    dma_set_memory_address(DMA2, 5, (u32) buffer);
+    dma_set_number_of_data(DMA2, 5, length);
+    dma_enable_stream(DMA2, 5);
+    return 0;
+}
+
+int beagle_spi_dma_rx(uint16_t length, char *buffer,
+                      completion_t rx_cb, void *user_data)
+{
+    if (rx_busy) return -1;
+    rx_busy = true;
+    rx_completion = rx_cb;
+    rx_user_data = user_data;
+
+    spi_enable_rx_dma(SPI1);
+    dma_set_memory_address(DMA2, 0, (u32) buffer);
+    dma_set_number_of_data(DMA2, 0, length);
+    dma_enable_stream(DMA2, 0);
+    return 0;
+}
+
+void dma2_stream5_isr() {
+    if (dma_get_interrupt_flag(DMA2, 5, DMA_ISR_TCIF)) {
+        dma_clear_interrupt_flags(DMA2, 5, DMA_ISR_TCIF);
+        spi_disable_tx_dma(SPI1);
+        tx_busy = false;
+    }
+    if (dma_get_interrupt_flag(DMA2, 5, DMA_ISR_TEIF)) {
+        dma_clear_interrupt_flags(DMA2, 5, DMA_ISR_TEIF);
+    }
+    if (dma_get_interrupt_flag(DMA2, 5, DMA_ISR_FEIF)) {
+        // This happens for some reason even when the FIFO is
+        // disabled. Regardless, it doesn't seem to do any harm.
+        dma_clear_interrupt_flags(DMA2, 5, DMA_ISR_FEIF);
+    }
 }
 
 void dma2_stream0_isr() {
     if (dma_get_interrupt_flag(DMA2, 0, DMA_ISR_TCIF)) {
         // Transfer complete
         dma_clear_interrupt_flags(DMA2, 0, DMA_ISR_TCIF);
-        dma_disable_stream(DMA2, 0);
         spi_disable_rx_dma(SPI1);
-        //process_cmd();
+        if (rx_completion)
+            rx_completion(rx_user_data);
     }
 }
 
--- beagle_spi.h
+++ beagle_spi.h
@@ -1,3 +1,10 @@
 #pragma once
 
 void beagle_spi_init();
+
+typedef void (*completion_t)(void *user);
+
+int beagle_spi_dma_tx(uint16_t length, char *buffer);
+int beagle_spi_dma_rx(uint16_t length, char *buffer,
+                      completion_t rx_cb, void *user_data);
+
