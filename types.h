39dfb0158bdb61571995612df33e24ab6fef1901
Ben Gamari <bgamari.foss@gmail.com>
2014-08-19 17:44:14 -0400

    document channel assignments

--- types.h
+++ types.h
@@ -8,6 +8,14 @@ typedef int32_t fixed16_t;
 typedef int32_t fixed24_t;
 
 // A set of samples from the ADC
+//   frame[0] = stage x
+//   frame[1] = stage y
+//   frame[2] = stage z
+//   frame[3] = sum x
+//   frame[4] = diff x
+//   frame[5] = sum y
+//   frame[6] = diff y
+//   frame[7] = unused
 typedef int16_t adc_frame_t[8];
 
 typedef fixed24_t adc_avg_frame_t[8];
